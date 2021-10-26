f6748afb20ca7f4f6e474e0b4b66ce6fc12557e5
Stanislav <denfas112@gmail.com>
2021-10-26 15:38:34 +0300

    Benches

---
+++ src/yandex/b2/Benches.java
@@ -0,0 +1,34 @@
+package yandex.b2;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Benches {
+
+    public static void main(String[] args) throws IOException {
+        Scanner inp = new Scanner(System.in);
+        int length = inp.nextInt(), benchesCount = inp.nextInt(), nextValue, prevValue = 0, center = (length - length % 2) / 2;
+        boolean isOdd = length % 2 == 1;
+        List<String> result = new LinkedList<>();
+
+        for (int i = 0; i < benchesCount; i++) {
+            nextValue = inp.nextInt();
+            if (isOdd && nextValue == (length - 1) / 2) {
+                result.add(String.valueOf(nextValue));
+                break;
+            }
+
+            if (prevValue <= center && nextValue >= center) {
+                result.add(String.valueOf(prevValue));
+                result.add(String.valueOf(nextValue));
+                break;
+            }
+
+            prevValue = nextValue;
+        }
+
+        System.out.println(String.join(" ", result));
+    }
+}
