6017107da3dda6ae56748a3d9f95c0b0bd577c1e
Stanislav <denfas112@gmail.com>
2021-11-05 01:01:30 +0300

    Customs v2

--- app/src/main/java/yandex/b7/Customs.java
+++ app/src/main/java/yandex/b7/Customs.java
@@ -1,6 +1,5 @@
 package yandex.b7;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -8,61 +7,29 @@ import java.util.Scanner;
 
 public class Customs {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int count = inp.nextInt(), numberOfCargo = 0, maxNumbersOfCargo = 0, timeIn, timeProcessing;
-        List<Event> events = new ArrayList<>(count * 2);
+        List<Integer[]> events = new ArrayList<>(count * 2);
         while (count > 0) {
             timeIn = inp.nextInt();
             timeProcessing = inp.nextInt();
-            events.add(new Event(Type.START, timeIn));
-            events.add(new Event(Type.END, timeIn + timeProcessing));
+            events.add(new Integer[]{1, timeIn});
+            events.add(new Integer[]{-1, timeIn + timeProcessing});
             count--;
         }
-        events.sort(Comparator.comparing(Event::getValue).thenComparing(Event::getType));
+        events.sort(((Comparator<Integer[]>) (o1, o2) -> o1[1].compareTo(o2[1])).thenComparing((o1, o2) -> o1[0].compareTo(o2[0])));
 
-        if(events.size() < 2){
+        if (events.size() < 2) {
             System.out.println(events.size());
             return;
         }
 
-        for (Event event : events) {
-            if (event.type.equals(Type.END)) {
-                numberOfCargo -= 1;
-            }
-            if (event.type.equals(Type.START)) {
-                numberOfCargo += 1;
-            }
+        for (Integer[] event : events) {
+            numberOfCargo += event[0];
             maxNumbersOfCargo = Math.max(maxNumbersOfCargo, numberOfCargo);
         }
 
-        System.out.println( maxNumbersOfCargo);
-    }
-
-    private enum Type {
-        END, START
-    }
-
-    private static class Event {
-        private final Type type;
-        private final int value;
-
-        public Event(Type type, int value) {
-            this.type = type;
-            this.value = value;
-        }
-
-        public Type getType() {
-            return type;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        @Override
-        public String toString() {
-            return "Event{type=" + type + ", value=" + value + '}';
-        }
+        System.out.println(maxNumbersOfCargo);
     }
 }
