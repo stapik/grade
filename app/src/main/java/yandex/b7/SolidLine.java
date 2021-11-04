0e5ae5251667062b06ad5c9cbc2af4d5888e3fbf
Stanislav <denfas112@gmail.com>
2021-11-04 14:17:23 +0300

    Solid Line

--- app/src/main/java/yandex/b7/SolidLine.java
+++ app/src/main/java/yandex/b7/SolidLine.java
@@ -1,21 +1,67 @@
 package yandex.b7;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
 
 public class SolidLine {
 
     public static void main(String[] args) throws IOException {
-        AtomicInteger sum = new AtomicInteger();
-        Files.lines(Paths.get("input.txt")).skip(1).forEach(row -> {
-            String[] numberList = row.split(" ");
-            if (numberList.length == 2) {
-                int a = Integer.parseInt(numberList[0]), b = Integer.parseInt(numberList[1]);
-                sum.addAndGet(Math.abs(b - a));
+        Scanner inp = new Scanner(System.in);
+        int count = inp.nextInt(), numberOfLayers = 0, length = 0, from = 0;
+        List<Event> events = new ArrayList<>(count * 2);
+        while (count > 0) {
+            events.add(new Event(Type.START, inp.nextInt()));
+            events.add(new Event(Type.END, inp.nextInt()));
+            count--;
+        }
+        events.sort(Comparator.comparing(Event::getValue).thenComparing(Event::getType));
+
+        for (Event event : events) {
+            if (event.type.equals(Type.START)) {
+                if (numberOfLayers == 0) {
+                    from = event.getValue();
+                }
+                numberOfLayers += 1;
             }
-        });
-        System.out.println(sum.get());
+
+            if (event.type.equals(Type.END)) {
+                numberOfLayers -= 1;
+                if (numberOfLayers == 0) {
+                    length += event.getValue() - from;
+                }
+            }
+        }
+
+        System.out.println(length);
+    }
+
+    private enum Type {
+        START, END
+    }
+
+    private static class Event {
+        private final Type type;
+        private final int value;
+
+        public Event(Type type, int value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Event{" +
+                    "type=" + type +
+                    ", value=" + value +
+                    '}';
+        }
     }
 }
