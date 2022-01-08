86489710cc32cd29f7d109de67041fdc8c2633d0
Stanislav <denfas112@gmail.com>
2022-01-08 13:21:35 +0300

    SubStrings v2

--- app/src/main/java/sberfight/SubStrings.java
+++ app/src/main/java/sberfight/SubStrings.java
@@ -1,17 +1,14 @@
 package sberfight;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class SubStrings {
 
     public static void main(String[] args) {
         System.out.println(getResult("aaa bbb", "a")); // 7
+        System.out.println(getResult("a a bb a", "a")); // 8
         System.out.println(getResult("bbbb cc aa", "b")); // 10
-        System.out.println(getResult("b", "b")); // 1
-        System.out.println(getResult("b b", "b")); // 3
+        System.out.println(getResult("aaaaaaaa  dddd bbb ccc", "b")); // 22
     }
 
     public static int getResult(String s, String symbol) {
@@ -27,42 +24,31 @@ public class SubStrings {
     }
 
     private static int calcMaxSubstring(String[] subStrings, int index, String symbol) {
-        int leftIdx = index - 1, rightIdx = index + 1, leftSize = 0, rightSize = 0;
-        Set<Integer> selectedSubstrings = new HashSet<>();
-        selectedSubstrings.add(index);
-
+        int leftIdx = index - 1, rightIdx = index + 1, totalSymbols = subStrings[index].length(), countSubstrings = 1;
         Predicate<Integer> indexExists = (idx) -> subStrings.length > idx && idx > -1;
-        Supplier<Integer> totalSymbols = () -> selectedSubstrings
-                .stream()
-                .mapToInt(i -> subStrings[i].length()).sum()
-                + selectedSubstrings.size() - 1;
+        boolean leftFlag = false, rightFlag = false;
 
         while (true) {
-            // -1 = all symbols, 0 = nothing, n > 0 = other symbols
-            if (indexExists.test(leftIdx)) {
-                leftSize = subStrings[leftIdx].contains(symbol) ? -1 : subStrings[leftIdx].length();
-            }
-            if (indexExists.test(rightIdx)) {
-                rightSize = subStrings[rightIdx].contains(symbol) ? -1 : subStrings[rightIdx].length();
-            }
-
-            if (leftSize == -1 || totalSymbols.get() >= leftSize && leftIdx > - 1 && leftSize > 0) {
-                selectedSubstrings.add(leftIdx);
+            if (indexExists.test(leftIdx) && (subStrings[leftIdx].contains(symbol) || subStrings[leftIdx].length() <= totalSymbols)) {
+                totalSymbols += subStrings[leftIdx].length();
+                countSubstrings++;
+                leftFlag = true;
                 leftIdx--;
             }
-            if (rightSize == -1 || totalSymbols.get() >= rightSize && rightIdx > -1 && rightSize > 0) {
-                selectedSubstrings.add(rightIdx);
+            if (indexExists.test(rightIdx) && (subStrings[rightIdx].contains(symbol) || subStrings[rightIdx].length() <= totalSymbols)) {
+                totalSymbols += subStrings[rightIdx].length();
+                countSubstrings++;
+                rightFlag = true;
                 rightIdx++;
             }
 
-            if (leftSize == 0 && rightSize == 0) {
+            if (!leftFlag && !rightFlag) {
                 break;
             }
-
-            leftSize = 0;
-            rightSize = 0;
+            leftFlag = false;
+            rightFlag = false;
         }
 
-        return totalSymbols.get();
+        return totalSymbols + countSubstrings - 1;
     }
 }
