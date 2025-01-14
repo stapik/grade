d6d800ae693561bee1947f5ef8f86474a0509519
Pikalov Stanislav <ext_spikalov@rencredit.ru>
2025-01-14 20:14:34 +0300

    Tourism

--- app/src/main/java/codeRun/Tourism.java
+++ app/src/main/java/codeRun/Tourism.java
@@ -1,7 +1,6 @@
 package codeRun;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class Tourism {
 
@@ -12,6 +11,7 @@ public class Tourism {
         int pointCount = Integer.parseInt(reader.readLine());
         int[][] pointsArr = new int[pointCount][2];
         String[] temp;
+
         for (int i = 0; i < pointCount; i++) {
             temp = reader.readLine().split(" ");
             pointsArr[i][0] = Integer.parseInt(temp[0]);
@@ -19,39 +19,36 @@ public class Tourism {
         }
 
         int[] prefixSumUp = new int[pointCount];
-        prefixSumUp[0] = 0;
-        int diff;
-        for (int i = 1; i < pointsArr.length; i++) {
-            diff = Math.max(pointsArr[i][1] - pointsArr[i - 1][1], 0);
+        for (int i = 1; i < pointCount; i++) {
+            int diff = Math.max(pointsArr[i][1] - pointsArr[i - 1][1], 0);
             prefixSumUp[i] = prefixSumUp[i - 1] + diff;
         }
 
         int[] prefixSumDown = new int[pointCount];
-        prefixSumDown[pointCount - 1] = 0;
-        for (int i = pointsArr.length - 2; i > 0; i--) {
-            diff = Math.max(pointsArr[i][1] - pointsArr[i + 1][1], 0);
+        for (int i = pointCount - 2; i >= 0; i--) {
+            int diff = Math.max(pointsArr[i][1] - pointsArr[i + 1][1], 0);
             prefixSumDown[i] = prefixSumDown[i + 1] + diff;
         }
 
         int trackCount = Integer.parseInt(reader.readLine());
-        int firstIdx, secondIdx;
-        int distance;
-        int[] result = new int[trackCount];
+        StringBuilder result = new StringBuilder();
+
         for (int i = 0; i < trackCount; i++) {
             temp = reader.readLine().split(" ");
-            firstIdx = Integer.parseInt(temp[0]) - 1;
-            secondIdx = Integer.parseInt(temp[1]) - 1;
+            int firstIdx = Integer.parseInt(temp[0]) - 1;
+            int secondIdx = Integer.parseInt(temp[1]) - 1;
 
+            int distance;
             if (firstIdx < secondIdx) {
                 distance = prefixSumUp[secondIdx] - prefixSumUp[firstIdx];
             } else {
                 distance = prefixSumDown[secondIdx] - prefixSumDown[firstIdx];
             }
-            result[i] = distance;
+            result.append(distance).append("\n");
         }
-        String[] resultStrArr = Arrays.toString(result).split("[\\[\\]]")[1].split(", ");
-        writer.write(String.join("\n", resultStrArr));
 
+        // Вывод результата
+        writer.write(result.toString());
         reader.close();
         writer.close();
     }
